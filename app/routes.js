4d44cb5aa5d85126a841fa46fdedf1a706734583
Mamadou SALL <sallmahdou1@gmail.com>
2025-07-02 06:10:24 +0200

    feat: Add user controller and root

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,11 @@
 // Check http://expressjs.com/fr/guide/routing.html for documentation
 
 const WelcomeController   = require('./controllers/WelcomeController');
+const UserController   = require('./controllers/UserController');
 
 //
+
 module.exports.init = (app) => {
   app.get('/', WelcomeController.index);
+  app.get('/users', UserController.index);
 };
