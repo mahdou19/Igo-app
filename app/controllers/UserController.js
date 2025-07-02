9430aed51a748ce9e34115bf57679b5d93bd5be7
Mamadou SALL <sallmahdou1@gmail.com>
2025-07-02 06:55:53 +0200

    Dsplay data on FE

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -3,11 +3,6 @@ const User = require('../models/User');
 exports.index = async function(req, res) {
   console.log('Create user');
 
-  await User.create({
-    name: 'test',
-    email:  'test@mail.com',
-  });
-  
   const users =  await User.list();
   console.log(users);
   res.render('users/index', { users });
